7841c58f0b3f298bbe2a81837ca037275e7ebf6e
Hassan Ihsan <ihsanhassan.business@gmail.com>
2024-09-14 16:03:11 -0700

    update: started working on the back-end server file

---
+++ back-end/server.js
@@ -0,0 +1,57 @@
+const express = require('express');
+const {MongoClient, ObjectId} = require('mongodb');
+const cors = require('cors');
+const app = express();
+const port = 3000;
+//const router = express.Router();
+
+require('dotenv').config()
+
+
+const mongoURI = process.env.CONNECTION_STRING;
+const client = new MongoClient(mongoURI, {useNewUrlParser: true, useUnifiedTopology:true});
+
+app.use(cors());
+app.use(express.json());
+app.use(express.urlencoded({ extended: true}));
+
+//*Connect to MongoDb
+client.connect(err => {
+    if(err){
+        console.error('Failed to connect to Mongo', err);
+        process.exit(1);
+    }
+    console.info("connect to Mongodb");
+});
+
+
+//*connect to DB
+//TODO: modify this so the Db is connecting to can be changed!
+//TODO: currently it is hardcoded to connect to NLIC_DATABASE
+async function connectToDB() {
+    if (!db) {
+      try {
+        await client.connect();
+        db = client.db('NLIC_DATABASE');
+
+        console.info('Connected to MongoDB');
+      } catch (err) {
+        console.error('Failed to connect to MongoDB', err);
+        throw new Error('Database connection failed');
+      }
+    }
+  }
+
+
+//*health check
+app.get('/', (req, res) => {
+    res.send("App is running... v1.0.0");
+    
+});
+
+
+ 
+app.listen(port, () => {
+  console.log(`Proxy server running at http://localhost:${port}`);
+});
+
